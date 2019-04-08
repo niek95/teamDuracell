d2ef902bdc1d0d19f0d60c47431045f4a97320cf
Niek <niek.slemmer@hotmail.com>
2019-04-08 16:55:46 +0200

    small changes

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from batteries import Batteries
 from houses import House
 
-with open("Huizen&Batterijen/wijk1_batterijen.txt", "r") as f: 
-    batteries = {}
+with open("Huizen&Batterijen/wijk1_batterijen.txt", "r") as f:
+    batteries = []
     id = 0
     for line in f:
         lines = line.split(',')
@@ -10,12 +10,12 @@ with open("Huizen&Batterijen/wijk1_batterijen.txt", "r") as f:
         y_battery = lines[1]
         max_input = lines[2].strip()
         max_input = float(max_input)
-        batterie = Batteries(id, x_battery, y_battery, max_input)
-        batteries[id] = batterie
+        battery = Batteries(id, x_battery, y_battery, max_input)
+        batteries.append(battery)
         id += 1
 
 with open("Huizen&Batterijen/wijk1_huizen.txt", "r") as g:
-    houses = {}
+    houses = []
     id = 0
     for house in g:
         house_info = house.split(',')
@@ -23,12 +23,10 @@ with open("Huizen&Batterijen/wijk1_huizen.txt", "r") as g:
         y_house = int(house_info[1])
         out_put = float(house_info[2])
         house = House(id, x_house, y_house, out_put)
-        houses[id] = house
-        id += 1    
-        for i in range(0,len(batteries)):
-            x_battery = batteries[i].x_bat
-            y_battery = batteries[i].y_bat 
-            house.add_route(x_battery, y_battery)      
-    
+        houses.append(house)
+        id += 1
+        for battery in batteries:
+            house.add_route(battery.id, battery.x_bat, battery.y_bat)
+
 for i in range(0,len(houses)):
     print(houses[i].routes)
--- batteries.py
+++ batteries.py
@@ -1,12 +1,12 @@
 class Batteries(object):
-    def __init__(self, indentification, x, y, in_put):
-        self.indentification = indentification
+    def __init__(self, id, x, y, in_put):
+        self.id = id
         self.x_bat = int(x)
         self.y_bat = int(y)
         self.in_put = float(in_put)
         self.routes = []
-        
-    def add_routes(self, x_house, y_house):
+
+    def add_route(self, x_house, y_house):
         x_distance = x_house - self.x_bat
         if x_distance < 0:
             x_distance *= -1
